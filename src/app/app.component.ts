8b76027a70e9aaac6f8633a05b52c71cfc97b4b8
Cristian Ninja <cristianferrarys@gmail.com>
2020-08-23 02:43:48 -0300

    init monitereo test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  charters :any[] = [];
+  charterSeleccionado :any ={};
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+  siguiendo: boolean = false;
+
+  title = 'Cristian ';
+  constructor(private afDB: AngularFireDatabase) {
+    afDB.list('/usuarios').subscribe ((charters)=>{
+      this.charters = charters;
+      if(this.siguiendo){
+        for(let charter of charters){
+          if(charter.$key == this.charterSeleccionado.$key){
+            this.lat = charter.lat;
+            this.lng = charter.lng;
+          }
+        }
+
+      }
+    });
+  
+    console.log(this.charters);
+  }
+
+
+  charter_seleccionado(charter:any){
+    this.lat = charter.lat;
+    this.lng = charter.lng;
+    this.siguiendo = true;
+    this.charterSeleccionado = charter;
+    
+  }
+  dejar_seguir(){
+    this.siguiendo = false;
+    this.charterSeleccionado = {};
+  }
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDAo2LQvMuqZzaLHD2AdEsOtvRmxh_YGrQ",
+  authDomain: "tracker-4fa62.firebaseapp.com",
+  databaseURL: "https://tracker-4fa62.firebaseio.com",
+  projectId: "tracker-4fa62",
+  storageBucket: "tracker-4fa62.appspot.com",
+  messagingSenderId: "720387898409"
+};
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyATQg58AFITG8yPzUwl6dPVypU4NoLug6M'
+    }),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
